3118657befb8e1591d9a8c245b64446268d3444d
ayusseth <er.ayushseth@gmail.com>
2025-09-25 11:38:49 +0530

    adding new files

---
+++ src/main/java/main/ApiGlobalValidationApplication.java
@@ -0,0 +1,13 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGlobalValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGlobalValidationApplication.class, args);
+	}
+
+}
