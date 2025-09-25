cfb44155cf3c46662452a09b85801ff7b77f12ab
ayusseth <er.ayushseth@gmail.com>
2025-09-25 11:42:16 +0530

    entity package is added inside it all entities like name ,email, age along with its annotation is added

--- src/main/java/main/entity/User.java
+++ src/main/java/main/entity/User.java
@@ -1,4 +1,25 @@
 package main.entity;
 
-public class User {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class User
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+
+    @Pattern(regexp = "^[A-Za-z0-9._%+-]+@gmail\\.com$",message = "email must be a valid Gmail address")
+    @Column(unique = true,nullable = false)
+    private String mail;
+
+    @Min(value = 18, message = "Age must be at least 18")
+    private int age;
 }
